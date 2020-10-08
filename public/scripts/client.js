1375ae42b777022fa500592c4ee1584f9b87acf3
Kaliza Anna <annkaliza@gmail.com>
2020-10-08 10:40:33 +0000

    add functionality to load tweet when new is submitted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,57 +4,53 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// convert time from millisecond
 
-   // convert time from millisecond
-
-   const convertTime = function(time) {
-    let temp;
-    if (time) {
-      const timeMin = Math.floor((Date.now() - Number(time)) / 60000);
-      if (timeMin < 1) {
-        return 'now';
-      } else if (timeMin === 1) {
-        return '1 minute ago';
-      } else if ( timeMin > 1 & timeMin < 60) {
-        return `${timeMin} minutes ago`;
-      } else if (timeMin >= 60 & timeMin < 1440) {
-        temp = Math.floor(timeMin / 60);
-        return `${temp} hours ago`;
-      } else {
-        temp = Math.floor(timeMin / 1440);
-        if (temp === 1) {
-          return `one day ago`;
-        } else if (temp < 30 ) {
-          return `${temp} days ago`;
-        } else {
-          temp = Math.floor(temp / 365);
-          if (temp === 1) {
-            return `one year ago`;
-          } else {
-            return `${temp} years ago`;
-            }
-            
-          }
-      }
-  
+const convertTime = function (time) {
+  let temp;
+  if (time) {
+    const timeMin = Math.floor((Date.now() - Number(time)) / 60000);
+    if (timeMin < 1) {
+      return "now";
+    } else if (timeMin === 1) {
+      return "1 minute ago";
+    } else if ((timeMin > 1) & (timeMin < 60)) {
+      return `${timeMin} minutes ago`;
+    } else if ((timeMin >= 60) & (timeMin < 1440)) {
+      temp = Math.floor(timeMin / 60);
+      return `${temp} hours ago`;
     } else {
-      return '';
+      temp = Math.floor(timeMin / 1440);
+      if (temp === 1) {
+        return `one day ago`;
+      } else if (temp < 30) {
+        return `${temp} days ago`;
+      } else {
+        temp = Math.floor(temp / 365);
+        if (temp === 1) {
+          return `one year ago`;
+        } else {
+          return `${temp} years ago`;
+        }
+      }
     }
-  };
-    
-  // read all tweets
+  } else {
+    return "";
+  }
+};
 
-  const renderTweets = function (tweets) {
-    for (let i = 0; i < tweets.length; i++) {
-      $("#view-tweet").append(createTweetElement(tweets[i]));
-    }
-  };
+// read all tweets
 
+const renderTweets = function (tweets) {
+  for (let i = 0; i < tweets.length; i++) {
+    $("#view-tweet").append(createTweetElement(tweets[i]));
+  }
+};
 
-  // read one tweet
+// read one tweet
 
-  const createTweetElement = function (tweetData) {
-    const $tweet = $(`
+const createTweetElement = function (tweetData) {
+  const $tweet = $(`
     <article id="view-tweet">
     <div class="content">
     <div class="tweet-header">
@@ -68,36 +64,29 @@
     <hr />
     <div class="tweet-footer">
       <h3>${convertTime(tweetData["created_at"])}</h3>
-      <h4>logos</h4>
     </div>
   </div>
   </article>
    `);
-    return $tweet;
-  };
+  return $tweet;
+};
 
+// load tweet from the server
 
-  // load tweet from the server
+const loadTweets = function () {
+  let $tweets;
+  $.ajax(" http://localhost:8080/tweets", {
+    method: "GET",
+    dataType: "JSON",
+  }).then(function (tweets) {
+    renderTweets(tweets);
+  });
 
-  const loadTweets = function () {
-    let $tweets;
-    $.ajax(" http://localhost:8080/tweets", { method: "GET", dataType: 'JSON'}).then(function (
-      tweets
-    ) {
-      renderTweets(tweets);
-    });
-
-    return $tweets;
-  };
-
+  return $tweets;
+};
 
 $(document).ready(function () {
   loadTweets();
-
-
-
-
-
 
   // send data to server
 
@@ -115,14 +104,13 @@ $(document).ready(function () {
       var url = form.attr("action");
 
       $.ajax({
-        type: "POST",
         url: url,
-        data: form.serialize(),
-        success: function (data) {
-          loadTweets(data);
-        },
+        method: "POST",
+
+        data: $("#tweet-text").serialize(),
+      }).then(function () {
+        loadTweets();
       });
     }
   });
-
 });
